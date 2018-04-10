42e48714be88195225f82b1dbe523cab7b026886
DNARoma <tangpei506@gmail.com>
2018-04-10 22:53:19 +0800

    fix event empty crash

--- src/routes/event.js
+++ src/routes/event.js
@@ -6,7 +6,7 @@ import mapToList from '../utils/mapToList';
 const api = 'event';
 const router = new Router();
 const eventList = Object.keys(dbMap).reduce((sum, region) => {
-  sum[region] = mapToList(dbMap[region].eventMap.entries).filter(elem => Number(elem.endAt) > Date.now());
+  sum[region] = mapToList(dbMap[region].eventMap.entries);
   return sum;
 }, {});
 // console.log(eventList);
@@ -15,32 +15,27 @@ const eventBadgeList = Object.keys(dbMap).reduce((sum, region) => {
   return sum;
 }, {});
 
-function getCurrEvent() {
-  const currentEvent = Object.keys(eventList).reduce((sum, region) => {
-    const currEvent = eventList[region].find(elem =>
-      (Number(elem.endAt) > Date.now() && Number(elem.startAt) < Date.now()) ||
-      (Number(elem.endAt) > Date.now() && Number(elem.startAt) > Date.now()))
-      || eventList[region][0];
-    if (currEvent.eventType === 'challenge') {
-      currEvent.detail = dbMap[region].challengeEventMap.entries[currEvent.eventId];
-    } else if (currEvent.eventType === 'story') {
-      currEvent.detail = dbMap[region].storyEventMap.entries[currEvent.eventId];
-    } else if (currEvent.eventType === 'versus') {
-      currEvent.detail = dbMap[region].versusEventMap.entries[currEvent.eventId];
-    } else if (currEvent.eventType === 'live_try') {
-      currEvent.detail = dbMap[region].liveTryEventMap.entries[currEvent.eventId];
-    }
-    sum[region] = currEvent;
-    return sum;
-  }, {});
-  return currentEvent;
+function getCurrEvent(region) {
+  const currEvent = eventList[region].find(elem =>
+    (Number(elem.endAt) > Date.now() && Number(elem.startAt) < Date.now()) ||
+    (Number(elem.endAt) > Date.now() && Number(elem.startAt) > Date.now()))
+    || eventList[region][0];
+  if (currEvent.eventType === 'challenge') {
+    currEvent.detail = dbMap[region].challengeEventMap.entries[currEvent.eventId];
+  } else if (currEvent.eventType === 'story') {
+    currEvent.detail = dbMap[region].storyEventMap.entries[currEvent.eventId];
+  } else if (currEvent.eventType === 'versus') {
+    currEvent.detail = dbMap[region].versusEventMap.entries[currEvent.eventId];
+  } else if (currEvent.eventType === 'live_try') {
+    currEvent.detail = dbMap[region].liveTryEventMap.entries[currEvent.eventId];
+  }
+  return currEvent;
 }
 
 router.prefix(`${apiBase}/${api}`);
 
 router.get('/', async (ctx, next) => {
-  const currentEvent = getCurrEvent();
-  ctx.body = currentEvent[ctx.params.server];
+  ctx.body = getCurrEvent(ctx.params.server);
   await next();
 });
 
