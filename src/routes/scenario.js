1b89dd756274b7338c0afb027e3afb0316ba6ce0
DNARoma <tangpei506@gmail.com>
2018-09-22 22:49:50 +0200

    fix scenario resource paths

--- src/routes/scenario.js
+++ src/routes/scenario.js
@@ -7,20 +7,20 @@ const router = new Router();
 
 router.prefix(`${apiBase}/${api}`);
 
-router.get('/chara/:name', async (ctx, next) => {
+router.get('/:assetBundleName/:name', async (ctx, next) => {
   try {
-    const remoteScenarioPath = `${remoteAddr}/assets-${ctx.params.server}/characters/resourceset/Scenario${ctx.params.name}.json`;
+    const remoteScenarioPath = `${remoteAddr}/assets-${ctx.params.server}/characters/resourceset/${ctx.params.assetBundleName}_rip/Scenario${ctx.params.name}.json`;
     const scenarioData = await (await fetch(remoteScenarioPath)).json();
     const ret = {};
     ret.env = {
-      backgroundImage: `assets/${scenarioData.firstLayout.firstBackgroundBundleName}_${scenarioData.firstLayout.firstBackground}.png`,
-      bgm: `assets/sound/scenario/bgm/${scenarioData.firstLayout.firstBgm}.mp3`,
+      backgroundImage: `assets/${scenarioData.firstLayout.firstBackgroundBundleName}_rip/${scenarioData.firstLayout.firstBackground}.png`,
+      bgm: `assets/sound/scenario/bgm/${scenarioData.firstLayout.firstBgm.toLowerCase()}_rip/${scenarioData.firstLayout.firstBgm}.mp3`,
     };
     ret.talk = scenarioData.talkData.Array.map(elem => ({
       text: elem.data.talkCharacters.body,
       charaId: elem.data.talkCharacters.Array[0].data.characterId,
       charaName: elem.data.talkCharacters.windowDisplayName,
-      voice: elem.data.voices.Array[0] ? `assets/sound/voice/scenario/resourceset/${elem.data.voices.Array[0].data.voiceId}.mp3` : null,
+      voice: elem.data.voices.Array[0] ? `assets/sound/voice/scenario/resourceset/${ctx.params.assetBundleName}_rip/${elem.data.voices.Array[0].data.voiceId}.mp3` : null,
     }));
 
     ctx.body = ret;
